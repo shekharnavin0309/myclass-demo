690d7f650c1e3fdbf1fb8a3adeb3c40c9ba4cbbb
shekharnavin0309 <shekhar.navin0309@outlook.com>
2024-02-22 19:44:01 +0530

    Added few files for teaching
    Added few files for teaching

---
+++ myTest.java
@@ -0,0 +1,15 @@
+public class myTest {
+
+    public int add(int x, int y){
+        return x + y;
+    }
+
+    public int sub(int x, int y){
+        return x - y;
+    }
+    
+    
+    public int mul(int x, int y){
+        return x * y;
+    }
+}
